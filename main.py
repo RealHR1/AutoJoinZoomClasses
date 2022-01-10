11e7efe74f7eae9d1c672604c4cc6f7010042995
RealHR <59334698+RealHR1@users.noreply.github.com>
2022-01-10 19:28:25 +0300

    v1.2

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import time
 import pandas as pd
 from datetime import datetime
 import subprocess
+from discord_webhook import DiscordWebhook, DiscordEmbed
 
 http_get_url = "YOUR IFTTT WEBHOOK URL HERE"
+discord_webhook_url = "YOUR DISCORD WEBHOOKS URL HERE"
 post_join_timeout = 60
 Code_rerun_timeout = 5
 startquery = 7
 checkdelay = 5
 
-print("Zoom Auto Class Join Bot v1.1")
+print("Zoom Auto Class Join Bot v1.2")
+
+webhook = DiscordWebhook(url=discord_webhook_url)
 
 def sign_in(meetingid, pswd):
     os.system('%APPDATA%\Zoom\\bin\Zoom.exe "--url=zoommtg://zoom.us/join?action=join&confno=' + str(meetingid) + '&pwd=' + str(pswd) + "\"")
@@ -35,10 +39,17 @@ while True:
         if "Zoom.exe" in tasklist_output:
             print(class_name + " class joined and class " + "has not" + " started")
             requests.get(http_get_url + "?value1=Bot has joined " + class_name + " class." + "&value2=Class has not started.")
-            subprocess.Popen("meetingstart.py" + " " + http_get_url + " " + checkdelay + " " + class_name, shell=True)
+
+            webhook.add_embed(DiscordEmbed(title='School', description='Bot has joined' + class_name + 'class. \nClass has not started.', color='ff007b'))
+            webhook.execute()
+            subprocess.Popen("meetingstart.py" + " " + http_get_url + " " + discord_webhook_url + " " + str(checkdelay) + " " + class_name, shell=True)
         else:
             print(class_name + " class joined and class " + "has" + " started")
             requests.get(http_get_url + "?value1=Bot has joined " + class_name + " class." + "&value2=Class has started.")
+
+            webhook.add_embed(DiscordEmbed(title='School', description='Bot has joined ' + class_name + ' class. \nClass has started.', color='ff007b'))
+            webhook.execute()
+
         time.sleep(post_join_timeout)
     else:
         time.sleep(Code_rerun_timeout)
